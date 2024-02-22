0f11a763e895aecb383db7f9de5e8e1f21e70d7b
hmvrs <hugomrserra@gmail.com>
2024-02-22 16:46:34 +0000

    Intial comit

--- Semana2/src/QuickUnionUF.java
+++ Semana2/src/QuickUnionUF.java
@@ -8,4 +8,21 @@ public class QuickUnionUF {
             id[i]=i;
         }
     }
+
+    public int root(int p){
+        while(p!=id[p]){
+            p=id[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q){          //are p and q connected?
+        return root(p)==root(q);
+    }
+
+    public void union(int p, int q){                 //connect p and q
+        int rp = root(p);
+        int rq = root(q);
+        id[rp]=rq;
+    }
 }
