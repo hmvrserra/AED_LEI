3a277dea7d40ca021a328d5c58f9e962191acace
fx0n <hugomrserra@gmail.com>
2024-02-23 18:49:12 +0000

    Updated 23/02 18:48

--- Semana2/src/QuickUnionUF.java
+++ Semana2/src/QuickUnionUF.java
@@ -2,27 +2,27 @@ public class QuickUnionUF {
 
     private int[] id;
 
-    public QuickUnionUF (int N) {
+    public QuickUnionUF (int N){        //constructor (N is the number of points)
         id = new int[N];
-        for (int i=0; i<N; i++) {
-            id[i]=i;
+        for (int i = 0; i<N; i++) {
+            id[i] = i;
         }
     }
 
-    public int root(int p){
+    public int root(int p){     //root of p
         while(p!=id[p]){
-            p=id[p];
+            p = id[p];
         }
         return p;
     }
 
-    public boolean connected(int p, int q){
+    public boolean connected(int p, int q){     //are p and q connected?
         return root(p)==root(q);
     }
 
-    public void union(int p, int q){
+    public void union(int p, int q){        //connect p and q
         int rp = root(p);
         int rq = root(q);
-        id[rp]=rq;
+        id[rp] = rq;
     }
 }
--- Semana2/src/WeightedQUPathCompressionUF.java
+++ Semana2/src/WeightedQUPathCompressionUF.java
@@ -1,2 +1,32 @@
 public class WeightedQUPathCompressionUF {
+
+    private int[] id;
+    private int[] sz;
+
+    public WeightedQUPathCompressionUF(int N){
+
+        id = new int[N];
+        for (int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
+    public int root(int p){
+        while (p != id[p]) {
+            p = id[p];
+        }
+        return p;
+    }
+
+    public boolean connected(int p, int q){
+        return root(p)==root(q);
+    }
+
+    public void union(int p, int q){
+        int rp = root(p);
+        int rq = root(q);
+        if (rp==rq)
+            return;
+
+    }
 }
