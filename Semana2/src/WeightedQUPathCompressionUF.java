51b6ba41c8845d2867eadae5de4c884685c94444
fx0n <hugomrserra@gmail.com>
2024-02-26 18:29:07 +0000

    Updated 26/02 18:29

--- Semana2/src/WeightedQUPathCompressionUF.java
+++ Semana2/src/WeightedQUPathCompressionUF.java
@@ -3,7 +3,7 @@ public class WeightedQUPathCompressionUF {
     private int[] id;
     private int[] sz;
 
-    public WeightedQUPathCompressionUF(int N){
+    public WeightedQUPathCompressionUF(int N){      //constructor (N is the number of points)
 
         id = new int[N];
         for (int i = 0; i < N; i++){
@@ -12,7 +12,7 @@ public class WeightedQUPathCompressionUF {
         }
     }
 
-    public int root(int p){
+    public int root(int p){     //root of p
         while (p != id[p]) {
             id[p] = id[id[p]];
             p = id[p];
@@ -20,11 +20,11 @@ public class WeightedQUPathCompressionUF {
         return p;
     }
 
-    public boolean connected(int p, int q){
+    public boolean connected(int p, int q){     //are p and q connected?
         return root(p)==root(q);
     }
 
-    public void union(int p, int q){
+    public void union(int p, int q){        //connect p and q
         int rp = root(p);
         int rq = root(q);
         if (rp==rq)
