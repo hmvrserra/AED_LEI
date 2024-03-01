e3a4eac6e00e004130566ef3ce511173206a17fb
fx0n <hugomrserra@gmail.com>
2024-03-01 19:24:14 +0000

    Updated 1/03 19:24

--- Semana3/src/ResizingArrayQueueOfStrings.java
+++ Semana3/src/ResizingArrayQueueOfStrings.java
@@ -70,7 +70,7 @@ public class ResizingArrayQueueOfStrings {
 
     /************************************************ EXTRA ****************************************************/
 
-    public void printArray() {
+    public void printArray(){
         System.out.print("[");
         for (int i = 0; i < size; i++) {
             if (i != 0) {
