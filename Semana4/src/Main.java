ce0b1569b8092fe4d69d0a21f2cda79a5e72b821
fx0n <hugomrserra@gmail.com>
2024-03-12 19:51:23 +0000

    Updated 12/03 19:51

--- Semana4/src/Main.java
+++ Semana4/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         Queue<Integer> queue = new Queue<>();
 
         // Enqueue some elements
--- Semana4/src/Queue.java
+++ Semana4/src/Queue.java
@@ -1,6 +1,5 @@
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 public class Queue<T> implements Iterable<T>{
 
     private Node first, last;
