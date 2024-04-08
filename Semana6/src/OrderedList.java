897e931df6de071a2d6dafe44585875c68b4876b
fx0n <hugomrserra@gmail.com>
2024-04-08 20:22:04 +0100

    Updated 8/04 20:22

--- Semana6/src/OrderedList.java
+++ Semana6/src/OrderedList.java
@@ -3,19 +3,59 @@ import java.util.NoSuchElementException;
 
 public class OrderedList<T extends Comparable<T>> implements Iterable<T> {
 
+    private Node first, last;
+    private int size;
 
-    //...
-
-    public void sort(){
-
+    public OrderedList(){
+        first = null;
+        last = null;
+        size = 0;
     }
 
-    public void shuffle(){
-
+    public void add(T item){
+        if (isEmpty()){
+            last = new Node(item, null, last);
+            first = last;
+        }else{
+            last.next = new Node(item, null, last);
+            last = last.next;
+        }
+        size++;
     }
+
+    public boolean contains(T item){
+        if (isEmpty())
+            return false;
+
+        for (Node current = first; current != null; current = current.next) {
+            if (current.item.equals(item))
+                return true;
+        }
+        return false;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+//    public void sort(){
+//
+//    }
+//
+//    public void shuffle(){
+//
+//    }
 
     public boolean isSorted(){
-
+        for (Node current = first; current != null; current=current.next ){
+            if (less(current.next.item, current.item))
+                return false;
+        }
+        return true;
     }
 
     private class Node{
@@ -28,6 +68,10 @@ public class OrderedList<T extends Comparable<T>> implements Iterable<T> {
             this.next = next;
             this.previous = previous;
         }
+    }
+
+    public Iterator<T> iterator(){
+        return new ListIterator();
     }
 
     private class ListIterator implements Iterator<T> {
@@ -48,5 +92,10 @@ public class OrderedList<T extends Comparable<T>> implements Iterable<T> {
     }
 
 
-}
+    /************************************* EXTRA ***********************************/
 
+    private boolean less(T a,T b){
+        return a.compareTo(b) < 0;
+    }
+
+}
