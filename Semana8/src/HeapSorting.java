c888eae08dfd5c92f402a3dbafb528b9897b763d
fx0n <hugomrserra@gmail.com>
2024-04-23 12:17:10 +0100

    Updated 23/04 12:17

--- Semana8/src/HeapSorting.java
+++ Semana8/src/HeapSorting.java
@@ -1,30 +1,56 @@
 public class HeapSorting {
 
     public static void sort(Comparable[] a){    //sorts the array a using the Heapsort
-
+        sortdown(a, a.length);
     }
 
-    public static void heapify(Comparable[] a, int N){
-
+    public static void heapify(Comparable[] a, int N){  //creates a max-heap with the N items in the array a
+        for (int k = N / 2; k >= 1; k--) {
+            sink(a, k, N);
+        }
     }
 
-    public static void sortdown(Comparable[] a, int N){
-
+    public static void sortdown(Comparable[] a, int N){ //repeatedly removes the max/biggest ite min the array
+        heapify(a, N);
+        while (N > 1){
+            exchange(a, 1, N--);
+            sink(a, 1, N);
+        }
     }
 
-    public static void sink(Comparable[] a, int k, int N){
-
+    public static void sink(Comparable[] a, int k, int N){  //sink node k
+        while (2 * k <= N){
+            int j = 2 * k;
+            if (j < N && less(a, j, j + 1))
+                j++;
+            if (!less(a, k, j))
+                break;
+            exchange(a, k, j);
+            k = j;
+        }
     }
 
-    public static int parent(int k){
-
+    public static int parent(int k){    //parent of node k
+        return (k - 1) / 2;
     }
 
-    public static int left(int k){
-
+    public static int left(int k){  //left child of node k
+        return 2 * k + 1;
     }
 
-    public static int right(int k){
+    public static int right(int k){ //right child of node k
+        return 2 * k + 2;
+    }
 
+    /************************************************** EXTRA **************************************************/
+
+    private static boolean less(Comparable[] a, int i, int j){
+        return a[i - 1].compareTo(a[j - 1]) < 0;
+    }
+
+    private static void exchange(Comparable[] a, int i, int j){
+        Comparable swap = a[i - 1];
+        a[i - 1] = a[j - 1];
+        a[j - 1] = swap;
     }
 }
--- Semana8/src/Main.java
+++ Semana8/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Integer[] a = {4, 2, 6, 7, 9, 8, 10, 3, 5, 1};
+        System.out.println("Before sorting:");
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        HeapSorting.sort(a);
+        System.out.println("\nAfter sorting:");
+        for (int i = 0; i < a.length; i++)
+            System.out.print(a[i] + " ");
+        System.out.println();
     }
 }
